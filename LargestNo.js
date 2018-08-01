48dac448b703698edbd307f8ab4e1a29f44ace2d
Darius Carter <dcarterbusinessemail@gmail.com>
2018-08-01 15:59:20 -0500

    Finished assignment

---
+++ LargestNo.js
@@ -0,0 +1,13 @@
+var myNumberList = [12, 2112, 9000, 1999, 24];
+var largest = 0;
+
+for(x = 0; x < myNumberList.length; x++)
+{
+    if (myNumberList[x]> largest){
+        largest = myNumberList[x];
+    }
+
+}
+console.log(largest);
+
+// I kinda cheated lol....looked up help on internet
